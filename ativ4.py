c707175cbffd8770f8a9f6d831e6351fab6e3cd1
DeiversonBorges <deiversonjob@gmail.com>
2024-09-11 09:54:50 -0300

    Update ativ4.py

--- ativ4.py
+++ ativ4.py
@@ -3,7 +3,7 @@
 
 l=input("Digite uma letra e eu direi se é volgal ou consoante: ")
 letra=l.lower().strip()
-if(letra=="a" or letra=="e" or letra=="i" or letra=="o" or letra=="u"):
+if(letra=="a" or letra=="e" or letra=="i" or letra=="o" or letra=="u"): #if letra in "aeiou":
     print(f"{l} é vogal")
 else:
     print(f"{l} é consoante")
