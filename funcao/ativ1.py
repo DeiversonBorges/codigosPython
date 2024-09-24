f5c40c7f6b482807f83232b0b2f29fed9e685834
DeiversonBorges <deiversonjob@gmail.com>
2024-09-24 17:06:00 -0300

    Delete funcao/ativ1.py

--- funcao/ativ1.py
+++
@@ -1,8 +0,0 @@
-def par_ou_impar(valor):
-    if(valor%2==0):
-        print("Par")
-    else:
-        print("Impar")
-
-valor=int(input("Digite um valor e eu direi se ele é par ou é impar: "))
-par_ou_impar(valor)
