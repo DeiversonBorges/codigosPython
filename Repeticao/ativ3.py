3308e6c3955c24c8c081cd7b5857f3a7abad9dcb
DeiversonBorges <deiversonjob@gmail.com>
2024-09-24 17:13:41 -0300

    Delete Repeticao directory

--- Repeticao/ativ3.py
+++
@@ -1,7 +0,0 @@
-multiplicando=int(input("Digite o multiplicando: "))
-multiplicador=int(input("Digite o multiplicador: "))
-produto=0
-for contador in range(multiplicador):
-    produto=produto+multiplicando
-
-print(f"O produto é igual a {produto}")
